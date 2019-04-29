f9186355726d42c742278cb6b67b507e6460cdbe
LogNeperien <helene.carlier-gubler@edu.ece.fr>
2019-04-29 14:58:58 +0200

    avec connection + blindage

--- connection.php
+++ connection.php
@@ -5,52 +5,74 @@
 $pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
 $email = isset($_POST["email"])? $_POST["email"] : "";
 
-
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+$erreur = "";
 
 
-//identifier le nom de la BDD
-$database = "Amazon";
-
-//se connecter à la BDD
-//$db_handle = mysql_connect(localhost,root,'');
-$db_handle = mysqli_connect(DB_SERVER, DB_USER ,DB_PASS);
-$db_found = mysqli_select_db($db_handle, $database);
-
-//si la BDD existe, faire le traitement
-if($db_found)
+//Si les champs ne sont pas remplis, on ajoute les erreurs
+if($pseudo=="")
 {
-	$sql = "SELECT * FROM Compte";
-			if ($email != "") {
+   $erreur .= "Le champ pseudo est vide. <br>";
+}
+if($email=="")
+{
+   $erreur .= "Le champ email est vide. <br>";
+}
+
+//s'il n'y a pas d'erreur
+if($erreur == "")
+{
+	define('DB_SERVER', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+
+
+	//identifier le nom de la BDD
+	$database = "Amazon";
+
+	//se connecter à la BDD
+	//$db_handle = mysql_connect(localhost,root,'');
+	$db_handle = mysqli_connect(DB_SERVER, DB_USER ,DB_PASS);
+	$db_found = mysqli_select_db($db_handle, $database);
+
+	//si la BDD existe, faire le traitement
+	if($db_found)
+	{
+		$sql = "SELECT * FROM Compte";
+		if ($email != "") {
 			//on cherche le compte avec les paramètres email et le pseudo
-				$sql .= " WHERE email LIKE '%$email%'";
-				if ($pseudo != "") {
-					$sql .= " AND pseudo LIKE '%$pseudo%'";
-				}
+			$sql .= " WHERE email LIKE '%$email%'";
+			if ($pseudo != "") {
+				$sql .= " AND pseudo LIKE '%$pseudo%'";
 			}
-			$result = mysqli_query($db_handle, $sql);
-			//regarder s'il y a de résultat
-			if (mysqli_num_rows($result) == 0) {
-				//le compte recherché n'existe pas
-				echo "email not found";
-			} 
-			else {
-				//on trouve le compte recherché
-				echo "
-				<form method=POST action=accueilClient.php>
-					<input type='hidden' name='email' value='<?php echo ' '.$email.' ' ?>'></input>
-				</form> ";
-				include('inscription.html');
-			}
-} //end if
-else //si la BDD n'existe passthru
-{
+		}
+		$result = mysqli_query($db_handle, $sql);
+		//regarder s'il y a de résultat
+		if (mysqli_num_rows($result) == 0) {
+			//le compte recherché n'existe pas
+			echo "email not found";
+		} 
+		else {
+			//on trouve le compte recherché
+			echo "
+			<form method=POST action=accueilClient.php>
+				<input type='hidden' name='email' value='<?php echo ' '.$email.' ' ?>'></input>
+			</form> ";
+			// lien pour renvoyer vers la page d'accueil de l'espace client
+			include('inscription.html');
+		}
+	} //end if
+	else //si la BDD n'existe passthru
+	{
 		echo "Database not found";
-} //end else
+	} //end else
 
-//fermer la connection
-mysqli_close($db_handle);
+	//fermer la connection
+	mysqli_close($db_handle);
+
+}
+else //s'il y a une erreur dans les champs
+{
+   echo $erreur;
+}
 
 ?>
