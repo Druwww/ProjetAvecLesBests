1916a262c1520697da0dbab1ba0120ec8e0f7926
inesestay <ines.estay@edu.ece.fr>
2019-05-03 16:03:08 +0200

    Fin achat

---
+++ fin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Amazon ECE</title>
+<link rel="icon" href="img/favicon.png" />
+	<link rel="stylesheet" type="text/css" href="styles13.css">
+	
+</head>
+<body>
+
+		<ul class="navigation1">
+				  <div class = "detail1">
+				  	<tr>
+						<button class="button button1">Mon Compte</button>
+					</tr>
+					<tr>
+						<img src="img/monCompte.png" style="width:50px;height:40px;" class = "detailImg">
+					</tr>
+				</div>
+				  
+		</ul>
+
+		<nav class="navbarCouleur"> 
+					<a href="AccueilClient.php"><img src="img/LogoSite.png" alt="Logo" class ="logo"></a> 
+				</nav>
+
+
+				<p class="slogan"> <img src="img/slogan.png" alt="slogan" style="width:500px;height:80;"></p>
+		
+	<center class = "fin"> <img src="img/fin.png" alt="fin" style = "height: 60px; width: 400px;"><br><br><img src="img/fin.gif" alt="fin" style = "height: 250px; width: 200px;"></center><br>
+	<center><a href="AcceuilClient.php"></a><button class="button button1">Revenir à l'accueil</button></center>
+
+<div class="footer">
+ <p><img src="img/coeur.png" style="width:50px;height:40px;" class = "detailImg"> &copy; 2019 Copyright | Amazon ECE<img src="img/coeur.png" style="width:50px;height:40px;" class = "detailImg"><p>
+</div>
+</body>
+</html>
