b1cb4ccf9ef96c209c912c09177dcd2cb51ac9ea
Druwww <ququq14@gmail.com>
2019-05-03 16:13:41 +0200

    ajout page confirmation achat

--- fin.php
+++ fin.php
@@ -28,7 +28,7 @@
 				<p class="slogan"> <img src="img/slogan.png" alt="slogan" style="width:500px;height:80;"></p>
 		
 	<center class = "fin"> <img src="img/fin.png" alt="fin" style = "height: 60px; width: 400px;"><br><br><img src="img/fin.gif" alt="fin" style = "height: 250px; width: 200px;"></center><br>
-	<center><a href="AcceuilClient.php"></a><button class="button button1">Revenir à l'accueil</button></center>
+	<center><a href="AccueilClient.php"><button class="button button1">Revenir à l'accueil</button></a></center>
 
 <div class="footer">
  <p><img src="img/coeur.png" style="width:50px;height:40px;" class = "detailImg"> &copy; 2019 Copyright | Amazon ECE<img src="img/coeur.png" style="width:50px;height:40px;" class = "detailImg"><p>
